dfb5629fabb6017da72de67d4a0d66bb78023546
Sara A <calisaraalhindi@gmail.com>
2020-09-03 03:40:11 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,8 +1,8 @@
 class User
-  attr_accessor :first, :last 
+  attr_accessor :first_name, :last_name 
   
-  def initialize(first,last)
-    @first= first
-    @last= last 
+  def initialize(first_name, last_name)
+    @first_name= first_name
+    @last_name= last_name 
   end
 end
