14fad63139d602ca142939b7247a24ca392b7e46
Sara A <calisaraalhindi@gmail.com>
2020-09-03 03:49:54 +0000

    Automatically backed up by Learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,7 +2,7 @@ class User
   attr_reader :first_name, :last_name 
   attr_writer :first_name, :last_name
   
-  def initialize(first_name, last_name)
+  def initialize
     @first_name= first_name
     @last_name= last_name 
   end
